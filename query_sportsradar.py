8758427cfcb754a587195dfd2b2f439fa73c1449
Will Weatherford <weatherford.william@gmail.com>
2016-09-10 13:52:03 -0700

    Basic structure.

--- query_sportsradar.py
+++ query_sportsradar.py
@@ -5,12 +5,28 @@ import sys
 import requests
 
 KEY = os.environ['SPORTSRADAR_API_KEY']
+ACCESS_LEVEL = 't'
+VERSION = '1'
+FORMAT = 'json'
+
+BASE_NFL_URL = 'https://api.sportradar.us/nfl-{}{}/games'.format(ACCESS_LEVEL, VERSION)
+
+GAME_ROUTE = '{game_id}/pbp.{format}'
 
 
-def main(*args):
+def main(game_id, *args):
     """Run requests against the SportsRadar API."""
 
-    requests.get()
+    params = {'api_key': KEY}
+    game_pbp_response = get_game_pbp(game_id, params)
+    print(game_pbp_response.json)
+
+
+def get_game_pbp(game_id, params):
+    """Get json for a given game ID."""
+    game_route = GAME_ROUTE.format(game_id=game_id, format=FORMAT)
+    url = '/'.join((BASE_NFL_URL, game_route))
+    return requests.get(url, params=params)
 
 
 if __name__ == '__main__':
